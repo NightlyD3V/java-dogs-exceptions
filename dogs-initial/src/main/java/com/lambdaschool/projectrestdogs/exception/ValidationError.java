2fd85b1189f6c632a60e2de56c9f37db17efd081
NightlyD3V <TylerSpaulding95@gmail.com>
2019-05-07 21:59:42 +0100

    Added exceptions and validation

--- dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/exception/ValidationError.java
+++ dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/exception/ValidationError.java
@@ -1,6 +1,28 @@
-package main.java.com.lambdaschool.projectrestdogs.exception;
+package com.lambdaschool.projectrestdogs.exception;
 
 public class ValidationError
 {
+    private String Code;
+    private String message;
+
+    public String getCode()
+    {
+        return Code;
+    }
+
+    public void setCode(String code)
+    {
+        Code = code;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
 
 }
